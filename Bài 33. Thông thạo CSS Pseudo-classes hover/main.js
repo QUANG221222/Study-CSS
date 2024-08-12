7ec902926672b2beb87e24e06fe97cc82218f0a8
QUANG221222 <nguyennhatquang.2509@gmail.com>
2024-08-12 20:54:33 +0700

    Update Knowledge 12/8

---
+++ Bài 33. Thông thạo CSS Pseudo-classes hover/main.js
@@ -0,0 +1,7 @@
+// const mouseOver = document.querySelector(".di_chuot");
+// mouseOver.addEventListener("mouseover",function()
+// {
+//     if(mouseOver instanceof HTMLElement)
+//         mouseOver.style.color = "cyan";
+// }
+// )
